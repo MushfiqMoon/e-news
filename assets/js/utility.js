845bf0e753b33b6ad481fffd53645885ca2d89c6
MushfiqMoon <mushfiq.moon013@google.com>
2022-09-03 07:41:43 +0600

    loading data succfully on dom

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -38,7 +38,7 @@ const displayNewsMenu = newsCat => {
     const allMenu = document.getElementById('main-menu')
 
     newsCat.forEach(menu => {
-        console.log(menu);
+        // console.log(menu);
         const menuItem = document.createElement('li')
         menuItem.classList.add('nav-item')
         menuItem.innerHTML = `
@@ -68,14 +68,38 @@ const loadNews = (id, name) => {
 
 const displayNews = (an) => {
     console.log(an.length);
-    const allNews = document.getElementById('all-news')
-
     const nfAmount = document.getElementById('nf-amount')
     nfAmount.innerText = `${an.length}`
 
+    const allNews = document.getElementById('all-news')
+
     an.map(news => {
         console.log(news);
+        const article = document.createElement('div')
+        article.classList.add('card')
+        article.classList.add('my-3')
+        article.innerHTML = `
 
+            <div class="row g-0">
+                <div class="col-md-3 p-3">
+                    <img src="${news?.thumbnail_url ? news?.thumbnail_url : "No Image Found"}" alt="${news.title}">
+                </div>
+                <div class="col-md-9">
+                    <div class="card-body p-5 ps-1">
+                        <h5 class="card-title">${news.title}</h5>
+                        <p class="card-text">${news.details}</p>
+                        <div class="d-flex justify-content-between">
+                            <div>Author</div>
+                            <div>${news?.total_view ? news?.total_view : 'No View Yet'}</div>
+                            <div>Rating</div>
+                            <div>BTN</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        `
+        allNews.appendChild(article)
     });
 
 }
